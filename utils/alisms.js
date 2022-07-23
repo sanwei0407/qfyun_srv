71aa4f733b00e85ce08ec2d24cb9e183fdc98418
sanwei0407 <77235159+sanwei0407@users.noreply.github.com>
2022-07-23 15:13:37 +0800

    Update alisms.js

--- utils/alisms.js
+++ utils/alisms.js
@@ -3,8 +3,8 @@
 const SMSClient = require('@alicloud/sms-sdk')
  
 // ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
-const accessKeyId = 'LTAI5tJpYFw8vXAF1wfpt7GW'
-const secretAccessKey = 'ps3dn1Ye4p1YCM3QBZpwhTK33l1iJv'
+const accessKeyId = 'LTAI5tJpYFw8vXAF1wfp222t7GW'
+const secretAccessKey = 'ps3dn1Ye4p1BZpwhTK33l1iJv'
  
 let smsClient = new SMSClient({accessKeyId, secretAccessKey});
 
